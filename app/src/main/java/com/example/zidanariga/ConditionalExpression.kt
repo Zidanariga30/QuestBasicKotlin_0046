095646cc9f7736e79542099a37921d65d5325a11
Apelicious <Apelicious@DESKTOP-7OGS3S6>
2024-10-13 00:13:31 +0700

    Menambahkan perulangan for untuk mencetak daftar tipe mobil dari list

--- app/src/main/java/com/example/zidanariga/ConditionalExpression.kt
+++ app/src/main/java/com/example/zidanariga/ConditionalExpression.kt
@@ -45,6 +45,12 @@ fun Perulangan() {
     for (i in 1..5) {
         println("Perulangan ke-$i")
     }
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars) {
+        println("Car type: $car")
+    }
+
 }
+
 
 
