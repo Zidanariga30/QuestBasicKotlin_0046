dc3868caeefc630af22650c1d4c6af36a96ec003
Apelicious <Apelicious@DESKTOP-7OGS3S6>
2024-10-12 23:53:48 +0700

    Menambahkan contoh implementasi Map Read-Only dan Mutable di Kotli. Menambahkan contoh penggunaan mapOf dan mutableMapOf dengan operasi tambah, ubah, dan hapus data. Implementasi map Read-Only dan Mutable dengan operasi dasar pada koleksi Map

--- app/src/main/java/com/example/zidanariga/Collections.kt
+++ app/src/main/java/com/example/zidanariga/Collections.kt
@@ -64,3 +64,42 @@ val shapesLocked: Set<String> = shape
 println(shapesLocked)
 }
 
+// Map adalah Kumpulan data yang disusun dalam pasangan key-value
+// Map bersifat unorfade, artinya data yang dimasukkan tidak meiliki posisi tertentu
+// Map Read-Only menggunakan mapOf
+// Map Mutable menggunakan mutableMapOf
+
+fun ContohMap() {
+
+    println()
+    println("=== Map ===")
+// Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+// Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam Map Mutable
+    shape["Rectang le"] = 4
+    println(shape)
+
+// Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+// Mengubah data di dalam Map Mutable
+    shape["Square"] = 5
+    println(shape)
+
+// Map Read-Only
+    val shapesLocked: Map<String, Int> = shape
+    println(shapesLocked)
+}
+
+fun main() {
+    ContohList()
+    ContohSet ()
+    ContohMap ()
+}
